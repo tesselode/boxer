a7b3bec61ba18a6947987517b99b1336638ce198
Andrew Minnich <aminnich3@gmail.com>
2019-01-20 23:47:37 -0500

    basic box properties

--- boxer.lua
+++ boxer.lua
@@ -27,15 +27,125 @@ local boxer = {
 	]]
 }
 
+local function count(...)
+	local amount = 0
+	for i = 1, select('#', ...) do
+		if select(i, ...) then amount = amount + 1 end
+	end
+	return amount
+end
+
+--[[
+	many properties in boxer can be set to either a raw value (number, string, etc.)
+	or a function that returns a raw value. this function will call the function if
+	necessary and return the raw value.
+]]
+local function get(value)
+    if type(value) == 'function' then
+        return value()
+    else
+        return value
+    end
+end
+
 local Box = {}
 
+-- gets a position along the x-axis of the box depending on the offset
+-- (0 = left, 0.5 = center, 1 = right, etc.)
+function Box:getX(offset)
+    offset = offset or 0
+    local x = get(self._x)
+    x = x - self.width * self._anchorX
+    return x + self.width * offset
+end
+
+-- gets a position along the y-axis of the box depending on the offset
+-- (0 = top, 0.5 = middle, 1 = bottom, etc.)
+function Box:getY(offset)
+    offset = offset or 0
+    local y = get(self._y)
+    y = y - self.height * self._anchorY
+    return y + self.height * offset
+end
+
+function Box:getRect()
+	return self.x, self.y, self.width, self.height
+end
+
+function Box:containsPoint(x, y)
+	return x >= self.left
+	   and x <= self.right
+	   and y >= self.top
+	   and y <= self.bottom
+end
+
+--[[
+	sets the position of a certain point along the x-axis of the box (defined by anchorX)
+	0 = left, 0.5 = center, 1 = right, etc.
+	x can be a number or a function that returns a number
+]]
+function Box:setX(x, anchorX)
+	anchorX = anchorX or 0
+	self._x, self._anchorX = x, anchorX
+end
+
+--[[
+	sets the position of a certain point along the y-axis of the box (defined by anchorY)
+	0 = top, 0.5 = middle, 1 = bottom, etc.
+	y can be a number or a function that returns a number
+]]
+function Box:setY(y, anchorY)
+	anchorY = anchorY or 0
+	self._y, self._anchorY = y, anchorY
+end
+
 function Box:__index(k)
+    if k == 'x' or k == 'left' then return self:getX(0) end
+    if k == 'center'           then return self:getX(.5) end
+    if k == 'right'            then return self:getX(1) end
+    if k == 'y' or k == 'top'  then return self:getY(0) end
+    if k == 'middle'           then return self:getY(.5) end
+    if k == 'bottom'           then return self:getY(1) end
+    if k == 'width'            then return get(self._width) end
+    if k == 'height'           then return get(self._height) end
+    return Box[k]
 end
 
 function Box:__newindex(k, v)
+    if k == 'x' or k == 'left' then
+        self:setX(v, 0)
+    elseif k == 'center' then
+        self:setX(v, .5)
+    elseif k == 'right' then
+        self:setX(v, 1)
+    elseif k == 'y' or k == 'top' then
+        self:setY(v, 0)
+    elseif k == 'middle' then
+        self:setY(v, .5)
+    elseif k == 'bottom' then
+        self:setY(v, 1)
+    elseif k == 'width' then
+        self._width = v
+    elseif k == 'height' then
+        self._height = v
+    else
+        rawset(self, k, v)
+    end
 end
 
-function boxer.box()
+function boxer.box(options)
+    if count(options.x, options.left, options.center, options.right) > 1 then
+        error('Cannot provide more than one horizontal position property', 2)
+    end
+    if count(options.y, options.top, options.middle, options.bottom) > 1 then
+        error('Cannot provide more than one vertical position property', 2)
+    end
+    local box = setmetatable({}, Box)
+    box.x, box.y, box.width, box.height = 0, 0, 0, 0
+    for k, v in pairs(options) do
+        box[k] = v
+    end
+    return box
 end
 
 return boxer
--- main.lua
+++ main.lua
@@ -1,32 +1,13 @@
 local boxer = require 'boxer'
 
-local box = boxer.Box {
-	x = 50,
+local box = boxer.box {
+	center = love.graphics.getWidth() / 2,
 	y = 50,
-	w = 100,
-	h = 100,
-	style = {
-		idle = {fillColor = {.2, .2, .2}},
-		hovered = {fillColor = {.4, .4, .4}},
-		pressed = {fillColor = {.3, .3, .3}},
-	},
-	onPress = function() print 'hi!' end,
+	width = function() return 100 + 100 * math.sin(love.timer.getTime()) end,
+	height = 100,
 }
-
-function love.mousemoved(...)
-	box:mousemoved(...)
-end
-
-function love.mousepressed(...)
-	box:mousepressed(...)
-end
-
-function love.mousereleased(...)
-	box:mousereleased(...)
-end
+print(box:getRect())
 
 function love.draw()
-	box:draw()
-	love.graphics.setColor(1, 1, 1)
-	love.graphics.print(math.floor(collectgarbage('count')) .. 'kb')
+	love.graphics.rectangle('fill', box:getRect())
 end
